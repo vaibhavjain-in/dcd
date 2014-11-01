d6d391f3c1593a9370585a59fa0e68b035ecdbef
Rohit Joshi <rohit.joshi@sdgc.com>
2014-11-01 14:54:00 +0530

    Added new database to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -569,3 +569,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 if (file_exists('/var/www/site-php')) {
   require '/var/www/site-php/drupalcampdelhi/drupalcampdelhi-settings.inc';
 }
+if (file_exists('/var/www/site-php')) {
+  require '/var/www/site-php/drupalcampdelhi/dcd2015_syncd-settings.inc';
+}
