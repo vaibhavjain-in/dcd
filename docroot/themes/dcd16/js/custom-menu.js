3135f00461a3554f8d0a9913fdb4abfee39ec4f5
Sumo <somedutta.ghosh@innoraft.com>
2016-09-19 01:55:39 +0530

    sliding menu framework set

---
+++ docroot/themes/dcd16/js/custom-menu.js
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * Responsive navigation tabs.
+ *
+ * This also supports collapsible navigable is the 'is-collapsible' class is
+ * added to the main element, and a target element is included.
+ */
+(function ($, Drupal) {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      $('#block-dcd16-main-menu').toggleClass('main-menu-show');
+    });
+  }
+
+  // $(document).ready(function($) {
+
+  // });
+
+})(jQuery, Drupal);
